2cef8e28f0854be95df3b1b144ec1ac2ef4b12a6
David Nascimento <davidpn.11@gmail.com>
2018-03-18 15:06:58 -0300

    create header filde

--- App.js
+++ App.js
@@ -5,33 +5,13 @@ import { Constants } from 'expo'
 import { MaterialIcons, Ionicons } from '@expo/vector-icons'
 import { isAndroid } from './utils/helpers'
 import Deck from './components/Deck'
-
-function MainStatusBar({ ...props }) {
-  return (
-    <View
-      style={{
-        backgroundColor: colorPrimary,
-        height: Constants.statusBarHeight,
-      }}
-    >
-      <StatusBar translucent stlye={styles.statusBar} {...props} />
-    </View>
-  )
-}
+import Header from './components/Header'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <MainStatusBar backgroundColor={colorPrimary} />
-        <View style={styles.toolbar}>
-          {isAndroid ? (
-            <MaterialIcons name="search" size={30} color={white} />
-          ) : (
-            <Ionicons name="ios-search" size={30} color={white} />
-          )}
-        </View>
-        <Deck />
+        <Header />
       </View>
     )
   }
@@ -40,21 +20,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  statusBar: {
-    backgroundColor: colorPrimary,
-    height: Constants.statusBarHeight,
-  },
-  toolbar: {
-    flex: 1,
-    height: 30,
-    flexDirection: 'row',
-    maxHeight: 50,
-    backgroundColor: colorPrimary,
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    paddingRight: 10,
-    shadowColor: 'rgba(0, 0, 0, 0.24)',
-    elevation: 2,
   },
 })
